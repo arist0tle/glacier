7c8e9bc05b8a95ad5b042c1afdb320df19e87342
thy_shenzhen <thy_shenzhen@163.com>
2020-03-20 22:37:32 +0800

    guava cache

--- glacier-server/src/main/java/com/geektcp/alpha/glacier/server/server/GlacierServiceImpl.java
+++ glacier-server/src/main/java/com/geektcp/alpha/glacier/server/server/GlacierServiceImpl.java
@@ -4,15 +4,18 @@ import com.geektcp.alpha.common.base.rpc.GlacierData;
 import com.geektcp.alpha.common.base.rpc.GlacierResponse;
 import com.geektcp.alpha.common.base.rpc.GlacierServiceGrpc;
 import com.geektcp.alpha.glacier.server.annotation.RpcService;
+import com.google.common.cache.*;
 import com.google.protobuf.ByteString;
 import io.grpc.stub.StreamObserver;
 import lombok.extern.slf4j.Slf4j;
 
+import javax.annotation.Nullable;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
 import java.util.Objects;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author tanghaiyang on 2020/3/15 15:50.
@@ -21,8 +24,31 @@ import java.util.Objects;
 @RpcService
 public class GlacierServiceImpl extends GlacierServiceGrpc.GlacierServiceImplBase {
 
+    private static RemovalListener<String, Long> myRemovalListener = new RemovalListener<String, Long>() {
+        @Override
+        public void onRemoval(RemovalNotification<String, Long> notification) {
+            String tips = String.format("key=%s,value=%s,reason=%s in myRemovalListener", notification.getKey(), notification.getValue(), notification.getCause());
+            log.info("tips: {} | onRemoval thread id: {}",tips, Thread.currentThread().getId());
+            if (notification.getCause().equals(RemovalCause.EXPIRED) && notification.getValue() != null) {
+                log.info("Remove {} in cacheConnection", notification.getKey());
+            }
+        }
+    };
+
+    public static LoadingCache<String, Long> CACHE = CacheBuilder.newBuilder()
+            .refreshAfterWrite(7, TimeUnit.SECONDS)
+            .expireAfterWrite(5, TimeUnit.SECONDS)
+            .removalListener(myRemovalListener)
+            .build(new CacheLoader<String, Long>() {
+                @Override
+                public Long load(@Nullable String key) throws Exception {
+                    return 100L;
+                }
+            });
+
     private static final String SAVE_PATH = "/data/server/";
     private static FileChannel fileChannel;
+    private static final String KEY_POSITION = "position";
 
     @Override
     public void send(GlacierData request, StreamObserver<GlacierResponse> responseObserver) {
@@ -46,13 +72,21 @@ public class GlacierServiceImpl extends GlacierServiceGrpc.GlacierServiceImplBas
             }
             ByteBuffer buffer = ByteBuffer.wrap(data.toByteArray());
             dstFileChannel.write(buffer, position);
-            builder.setPosition(dstFileChannel.position());
+            long writePosition = dstFileChannel.position();
+            builder.setPosition(writePosition);
+            CACHE.put(KEY_POSITION, writePosition);
             responseObserver.onNext(builder.build());
             responseObserver.onCompleted();
         } catch (Exception e) {
             log.info("fileName: {} | status:{}", fileName, status, e);
         }
 
+    }
+
+
+    @Override
+    public void locate(GlacierData request, StreamObserver<GlacierResponse> responseObserver) {
+        CACHE.getIfPresent(KEY_POSITION);
     }
 
     //////////////////////
